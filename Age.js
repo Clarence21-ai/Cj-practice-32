7b15b368889a3688c16f8b511da3f8c4e99e4199
Clarence21-ai <agtualclarence16@gmail.com>
2024-11-07 15:37:24 +0000

    Initial commit with ap.js

---
+++ Age.js
@@ -0,0 +1,15 @@
+let age = 25; 
+
+if (age < 5) {
+    console.log("The user is a Toddler (under 5 years).");
+    } else if (age >= 5 && age <= 12) {
+        console.log("The user is a Child (5–12 years).");
+        } else if (age >= 13 && age <= 19) {
+            console.log("The user is a Teenager (13–19 years).");
+            } else if (age >= 20 && age <= 35) {
+                console.log("The user is a Young Adult (20–35 years).");
+                } else if (age >= 36 && age <= 60) {
+                    console.log("The user is Middle-Aged (36–60 years).");
+                    } else {
+                        console.log("The user is a Senior (over 60 years).");
+                        }
